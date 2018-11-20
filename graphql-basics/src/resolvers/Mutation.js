7273661caecaf7725c23b382c28e26342c1fb025
Kirankumar Ahir <kirankatariya8778@gmail.com>
2018-11-20 18:50:43 +0530

    Adding comment edits and deletions subscription

--- graphql-basics/src/resolvers/Mutation.js
+++ graphql-basics/src/resolvers/Mutation.js
@@ -87,29 +87,41 @@ const Mutation = {
         db.posts.push(post);
         if(args.data.published) {
             pubsub.publish('post', {
-                post
+                post: {
+                    mutation: 'CREATED',
+                    data: post
+                }
             });
         }
 
         return post;
     },
     deletePost(parent, args, {
-        db
+        db, pubsub
     }, info) {
         const postIndex = db.posts.findIndex((post) => post.id === args.id);
 
         if (postIndex === -1) {
             throw new Error('Post not found!');
         }
-        const deletedPosts = db.posts.splice(postIndex, 1);
+        const [post] = db.posts.splice(postIndex, 1);
 
         db.comments = db.comments.filter((comment) => comment.post !== args.id);
 
-        return deletedPosts[0];
+        if(post.published) {
+            pubsub.publish('post', {
+                post: {
+                    mutation: 'DELETED',
+                    data: post
+                }
+            })
+        }
+        return post;
     },
-    updatePost(parent, args, { db }, info) {
+    updatePost(parent, args, { db, pubsub }, info) {
         const { id, data} = args;
         const post = db.posts.find((post) => post.id === id);
+        const originalPost = {...post};
 
         if(!post) {
             throw new Error("Post not found!");
@@ -125,6 +137,29 @@ const Mutation = {
 
         if(typeof data.published === 'boolean') {
             post.published = data.published;
+
+            if(originalPost.published && !post.published) {
+                pubsub.publish('post', {
+                    post: {
+                        mutation: 'DELETED',
+                        data: originalPost
+                    }
+                });
+            } else if(!originalPost.published && post.published) {
+                pubsub.publish('post', {
+                    post: {
+                        mutation: 'CREATED',
+                        data: post
+                    }
+                });
+            } 
+        } else if(post.published) {
+            pubsub.publish('post', {
+                post: {
+                    mutation: 'UPDATED',
+                    data: post
+                }
+            });
         }
 
         return post;
@@ -146,11 +181,14 @@ const Mutation = {
 
         db.comments.push(comment);
         pubsub.publish(`comment ${args.data.post}`, {
-            comment
+            comment: {
+                mutation: 'CREATED',
+                data: comment
+            }
         });
         return comment;
     },
-    updateComment(parent, args, { db }, info) {
+    updateComment(parent, args, { db, pubsub }, info) {
         const { id, data } = args;
         const comment = db.comments.find((comment) => comment.id === id);
         
@@ -162,10 +200,17 @@ const Mutation = {
             comment.text = data.text;
         }
         
+        pubsub.publish(`comment ${comment.post}`, {
+            comment: {
+                mutation: 'UPDATED',
+                data: comment
+            }
+        });
+
         return comment;
     },
     deleteComment(parent, args, {
-        db
+        db, pubsub
     }, info) {
         const commentIndex = db.comments.findIndex((comment) => comment.id === args.id);
 
@@ -173,8 +218,16 @@ const Mutation = {
             throw new Error('Comment not found');
         }
 
-        const deletedComments = db.comments.splice(commentIndex, 1);
-        return deletedComments[0];
+        const [deletedComment] = db.comments.splice(commentIndex, 1);
+
+        pubsub.publish(`comment ${deletedComment.post}`, {
+            comment: {
+                mutation: 'DELETED',
+                data: deletedComment
+            }
+        })
+
+        return deletedComment;
     }
 };
 
